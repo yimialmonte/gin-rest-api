9153caf9d9c34f2cfab75c851050d825ca1e378c
yimialmonte <yelmialmonte@gmail.com>
2021-04-29 21:49:27 -0400

    created video controller

---
+++ controller/video-controller.go
@@ -0,0 +1,32 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/yimialmonte/gin-rest-api/entity"
+	"github.com/yimialmonte/gin-rest-api/service"
+)
+
+type VideoController interface {
+	FindAll() []entity.Video
+	Save(ctx *gin.Context) entity.Video
+}
+
+type controller struct {
+	service service.VideoService
+}
+
+func New(s service.VideoService) VideoController {
+	return &controller{
+		service: s,
+	}
+}
+
+func (c *controller) FindAll() []entity.Video {
+	return c.service.FindAll()
+}
+
+func (c *controller) Save(ctx *gin.Context) entity.Video {
+	var video entity.Video
+	ctx.BindJSON(&video)
+	return c.service.Save(video)
+}
